ef60afd6eaf1b73e42cccf1da9a686f0822f2fcc
Fraser Murray <fraser.m.murray@gmail.com>
2014-09-15 18:15:33 +0100

    add a MatchSummary type

--- src/League/Types/Match.hs
+++ src/League/Types/Match.hs
@@ -41,6 +41,37 @@ instance FromJSON MatchDetail where
                 <*> o .: "matchVersion"
   parseJSON _ = mempty
 
+data MatchSummary = MatchSummary
+  { _match_summaryMatchID :: MatchID
+  , _match_summaryRegion :: Region
+  , _match_summaryCreationTime :: Integer
+  , _match_summaryDuration :: Integer
+  , _match_summaryMode :: MatchMode
+  , _match_summarySeason :: Season
+  , _match_summaryVersion :: Version }
+  deriving (Show, Read, Eq)
+
+instance Ord MatchSummary where
+  compare = compare `on` _match_summaryMatchID
+
+instance FromJSON MatchSummary where
+  parseJSON (Object o) =
+    MatchSummary <$> o .: "matchId"
+                 <*> o .: "region"
+                 <*> o .: "matchCreation"
+                 <*> o .: "matchDuration"
+                 <*> o .: "matchMode"
+                 <*> o .: "season"
+                 <*> o .: "matchVersion"
+  parseJSON _ = mempty
+
+data MatchSummaries = MatchSummaries [MatchSummary]
+  deriving (Show, Read, Eq)
+
+instance FromJSON MatchSummaries where
+  parseJSON (Object o) = MatchSummaries <$> o .: "matches"
+  parseJSON _ = mempty
+
 data Season = PreSeason3
             | Season3
             | PreSeason4
@@ -59,3 +90,4 @@ instance FromJSON Season where
   parseJSON _ = mempty
 
 makeFields ''MatchDetail
+makeFields ''MatchSummary
